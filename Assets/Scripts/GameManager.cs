35d63735b94a8e404814456f392651f2e9fae08f
jroca7 <70997708+jroca7@users.noreply.github.com>
2021-06-30 02:11:25 -0300

    Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour 
+{
+    public static int Score = 0;
+    public string ScoreString = "Score";
+
+    public Text TextScore;
+
+    public static GameManager Gamemanager;
+
+    public LevelBuilder levelBuilder;
+    public GameObject nextButton;
+    private bool readyForInput;
+    private Player player;
+
+    void Awake()
+    {
+        Gamemanager = this;
+    }
+    void Start() {
+        nextButton.SetActive(false);
+        ResetScene();
+        GameManager.Score = 0;
+    }
+
+    void Update() {
+
+        if(TextScore != null)
+        {
+            TextScore.text = ScoreString + Score.ToString();
+        }
+
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        input.Normalize();
+
+        if (input.sqrMagnitude > 0.5) {
+            if (readyForInput) {
+                readyForInput = false;
+                player.Move(input);
+                nextButton.SetActive(IsLevelComplete());
+            }
+        }
+        else {
+            readyForInput = true;
+        }
+    }
+
+    public void NextLevel() {
+        nextButton.SetActive(false);
+        levelBuilder.NextLevel();
+        GameManager.Score = 0;
+        StartCoroutine(ResetSceneAsync());
+    }
+
+    public void ResetScene() {
+        StartCoroutine(ResetSceneAsync());
+    }
+
+    bool IsLevelComplete() {
+        Box[] boxes = FindObjectsOfType<Box>();
+        foreach (var box in boxes) {
+            if (!box.onCrox) return false;
+        }
+        return true;
+    }
+
+    IEnumerator ResetSceneAsync() {
+        if (SceneManager.sceneCount > 1) {
+            AsyncOperation async = SceneManager.UnloadSceneAsync("LevelScene");
+            while (!async.isDone) {
+                yield return null;
+                
+            }
+            
+            Resources.UnloadUnusedAssets();
+        }
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("LevelScene", LoadSceneMode.Additive);
+        while (!asyncLoad.isDone) {
+            yield return null;
+            
+        }
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("LevelScene"));
+        levelBuilder.Build();
+        player = FindObjectOfType<Player>();
+        
+    }
+}
---
+++ Assets/Scripts/LevelBuilder.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class LevelElement {
+
+    public string character;
+
+    public GameObject prefab;
+}
+
+public class LevelBuilder : MonoBehaviour {
+    public int currentLevel;
+    public List<LevelElement> elements;
+    private Level level;
+
+    GameObject GetPrefab(char c) {
+        LevelElement levelElement = elements.Find(le => le.character == c.ToString());
+        if (levelElement != null) {
+            return levelElement.prefab;
+        }
+        else {
+            return null;
+        }
+    }
+
+    public void NextLevel() {
+        currentLevel++;
+        if (currentLevel >= GetComponent<Levels>().levels.Count) {
+            currentLevel = 0;
+        }
+    }
+
+    public void Build() {
+        level = GetComponent<Levels>().levels[currentLevel];
+        int startX = -level.width / 2;
+        int x = startX;
+        int y = -level.height / 2;
+        foreach (var row in level.rows) {
+            foreach (var c in row) {
+    //             if (c == '3') {
+    //                 GameObject prefabM = GetPrefab('2');
+    //               GameObject prefabE = GetPrefab('1');
+    //                if (prefabM != null && prefabE != null) {
+    //                  Instantiate(prefabM, new Vector3(x, y, 0), Quaternion.identity);
+    //                Instantiate(prefabE, new Vector3(x, y, 0), Quaternion.identity);
+    //          }
+    //    }
+                 if (c == '5') {
+                    GameObject prefabM = GetPrefab('5');
+                    GameObject prefabE = GetPrefab('0');
+                    if (prefabM != null && prefabE != null) {
+                        Instantiate(prefabM, new Vector3(x, y, 0), Quaternion.identity);
+                        Instantiate(prefabE, new Vector3(x, y, 0), Quaternion.identity);
+                    }
+                }
+                else if (c == '2') {
+                    GameObject prefabM = GetPrefab('2');
+                    GameObject prefabE = GetPrefab('0');
+                    if (prefabM != null && prefabE != null) {
+                        Instantiate(prefabM, new Vector3(x, y, 0), Quaternion.identity);
+                        Instantiate(prefabE, new Vector3(x, y, 0), Quaternion.identity);
+                    }
+                }
+                else if (c == '4') {
+                    GameObject prefabM = GetPrefab('4');
+                    GameObject prefabE = GetPrefab('0');
+                    if (prefabM != null && prefabE != null) {
+                        Instantiate(prefabM, new Vector3(x, y, 0), Quaternion.identity);
+                        Instantiate(prefabE, new Vector3(x, y, 0), Quaternion.identity);
+                    }
+                }
+                else {
+                    GameObject prefab = GetPrefab(c);
+                    if (prefab != null) {
+                        Instantiate(prefab, new Vector3(x, y, 0), Quaternion.identity);
+                    }
+                }
+                x++;
+            }
+            y++;
+            x = startX;
+        }
+    }
+
+}
